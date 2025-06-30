0491e128090d34377582e19f54c11e66dc188f0b
Lusther <l9.lemaire@gmail.com>
2025-07-01 00:29:32 +0200

    feat: Add starter for frontend

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 # roleplay-manager
 A tool to play tabletop roleplay games
+
+Run Backend
+```
+cd backend
+uvicorn app.main:app --reload
+```
+
+Run Frontend
+```
+cd frontend
+npm run dev
+```
---
+++ frontend/app.vue
@@ -0,0 +1,22 @@
+
+
+<!-- <template>
+  <div>
+    <NuxtRouteAnnouncer />
+    <NuxtWelcome />
+  </div>
+</template> -->
+
+<script setup lang="ts">
+const config = useRuntimeConfig()
+const { data } = await useFetch(`${config.public.apiBase}/`)
+</script>
+
+<template>
+  <div v-if="data == null">
+    No data
+  </div>
+  <div v-else>
+    {{ data }}
+  </div>
+</template>
